2cf501b8288fd4514df5a0133dfec104def615fb
nikshev <nikshev81@gmail.com>
2017-08-22 02:20:58 -0400

    Add Kafka Spec

--- app/services/KafkaService.scala
+++ app/services/KafkaService.scala
@@ -18,7 +18,7 @@ object KafkaService  {
     * @return
     */
    def send(key:String, value:String) = {
-   val  props = new Properties()
+    val  props = new Properties()
     val server =config.getString("kafka.bootstrapServer")+":"+config.getString("kafka.bootstrapServerPort")
     props.put("bootstrap.servers",server) //Docker return container id and you must add container id to hosts
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
@@ -26,6 +26,7 @@ object KafkaService  {
     props.put("acks", "all")
     props.put("retries", "3")
     props.put("linger.ms", "1")
+    props.put("group.id", "income")
 
     val producer = new KafkaProducer[String, String](props)
 
@@ -34,6 +35,7 @@ object KafkaService  {
 
     producer.close()
   }
+
 }
 
 
---
+++ test/kafka/KafkaServiceSpec.scala
@@ -0,0 +1,40 @@
+package kafka
+
+import db.phantom.entity.Income
+import org.joda.time.{DateTime, DateTimeZone}
+import org.scalatest.{FlatSpec, Inspectors, Matchers, OptionValues}
+import org.scalatest.concurrent.ScalaFutures
+import services.KafkaService
+
+
+class KafkaServiceSpec extends FlatSpec with Matchers with Inspectors with ScalaFutures with OptionValues{
+
+  //Record UUID using for tests
+  val recordUUID=java.util.UUID.randomUUID
+
+  //Income class ussed for tests
+  val record=Income.apply(
+    id=recordUUID,
+    symbol="EURUSD",
+    timeframe="1M",
+    date="2017-08-18",
+    time="11:13",
+    open=1.2732,
+    high=1.2732,
+    low=1.2732,
+    close=1.2732,
+    indicators=List(1,2,3,4,5,6,7,8,9,10),
+    levelUp=1.2732,
+    correctionLevelUp=1.2732,
+    levelDown=1.2732,
+    correctionLevelDown=1.2732,
+    prediction=0,
+    label=0,
+    createdAt=new DateTime(DateTimeZone.UTC)
+  )
+
+  "Send message to Kafka" should " must be successful" in {
+    KafkaService.send(record.hashCode().toString, record.toString())
+  }
+
+}
